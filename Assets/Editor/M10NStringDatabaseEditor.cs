89fb3efdd22c25d72d26bd4f0ba244b83b5f3bd1
Hiroki Omae <hiroki@unity3d.com>
2016-05-10 15:05:00 +0100

    add custom inspector

---
+++ Assets/Editor/M10NStringDatabaseEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(M10NStringDatabase))]
+public class M10NStringDatabaseEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+
+        M10NStringDatabase db = target as M10NStringDatabase;
+        
+        EditorGUILayout.LabelField("You don't wanna know...");
+
+		DrawDefaultInspector();
+    }
+}
