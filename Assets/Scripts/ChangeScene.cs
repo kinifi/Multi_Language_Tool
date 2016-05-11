70a426f2dfe7c30edd3ab1814083cbe547ab29b0
Chris Figueroa <chris@kinifigames.com>
2016-05-11 12:35:18 -0700

    added nightmare scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string levelToLoad;
+
+
+	public void changeSceneNow()
+	{
+		if(levelToLoad != null || levelToLoad != "" || levelToLoad != " ")
+		{
+			Application.LoadLevel(levelToLoad);
+		}
+	}
+
+}
