8e476858c865e4787c91193be1c48ca1dfbfd589
Chris Figueroa <chris@kinifigames.com>
2016-05-11 13:13:48 -0700

    more language changes

--- Assets/_CompletedAssets/Scripts/Managers/ScoreManager.cs
+++ Assets/_CompletedAssets/Scripts/Managers/ScoreManager.cs
@@ -25,7 +25,7 @@ namespace CompleteProject
         void Update ()
         {
             // Set the displayed text to be the word "Score" followed by the score value.
-            text.text = "Score: " + score;
+            text.text += (": " + score);
         }
     }
 }
